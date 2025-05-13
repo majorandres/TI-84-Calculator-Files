d3395954d7972d7267edf63203bfeda35bd203f7
Andres Popoca <apopo4@uic.edu>
2025-05-12 19:25:58 -0500

    Added Volume Solver
    Added basic volume equation solver for a good bunch of shapes.

--- equations.py
+++ equations.py
@@ -19,6 +19,8 @@ def userMenu():
             pythagSolver()
         elif(userInput == 2):
             areaSolver()
+        elif(userInput == 3):
+            volumeSolver()
         print("____________________________________________________________")
         print("0: Exit")
         print("1: Pythagorean Equation Solver")
@@ -79,6 +81,57 @@ def areaSolver():
         height = int(input("Enter the height of the trapezoid: "))
         print("Area of the trapezoid is " + str(((topBase + bottomBase) / 2) * height))
 
+
+def volumeSolver():
+    print("Please select some of the shapes from below to solve for the volume: ")
+    print("1: Cube")
+    print("2: Rectangular Prism")
+    print("3: Cone")
+    print("4: Triangular Pyramid")
+    print("5: Triangular Prism")
+    print("6: Square Prism")
+    print("7: Cylinder")
+    print("8: Sphere")
+    userInput = int(input("Enter the input here: "))
+    if(userInput == 1):
+       side = int(input("Enter a side from the Cube: "))
+       print("Volume of the cube is " + str(math.pow(side,3)))
+       
+    elif(userInput == 2):
+       length = int(input("Enter the length of the Rectangular Prism: "))
+       width = int(input("Enter the width of the Rectangular Prism: "))
+       height = int(input("Enter the height of the Rectangular Prism: "))
+       print("Volume of the rectangular prism is " + str(length * width * height))
+       
+    elif(userInput == 3):
+       base = int(input("Enter the base of the cone: "))
+       height = int(input("Enter the height of the cone: "))
+       print("Volume of the cone is " + str((1/3) * base * height))
+       
+    elif(userInput == 4):
+       base = int(input("Enter the base of the Triangular Pyramid: "))
+       height = int(input("Enter the height of the Triangular Pyramid: "))
+       print("Volume of the Triangular Pyramid is " + str((1/3) * base * height))
+       
+    elif(userInput == 5):
+       length = int(input("Enter the length of the Triangular Prism: "))
+       width = int(input("Enter the width of the Triangular Prism: "))
+       height = int(input("Enter the height of the Triangular Prism: "))
+       print("Volume of the Triangular Prism is " + str(length * width * height))
+       
+    elif(userInput == 6):
+       base = int(input("Enter the base of the Square Prism: "))
+       height = int(input("Enter the height of the Square Prism: "))
+       print("Volume of the Square Prism is " + str((1/3) * base * height))
+       
+    elif(userInput == 7):
+       base = int(input("Enter the base of the Cylinder: "))
+       height = int(input("Enter the height of the Cylinder: "))
+       print("Volume of the Cylinder is " + str(base * height))
+    elif(userInput == 8):
+        radius = int(input("Enter the radius of the Sphere: "))
+        print("Volume of a Sphere is " + str(((4/3) * math.pi * (math.pow(radius,3)))))
+    
 #* Start of the main program for the equation file
 def main():
    userMenu()
